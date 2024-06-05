d85a8d62f52166618130c4a7af3177033d9ffeb8
Duong Tri Nguyen <21130456@st.hcmuaf.edu.vn>
2024-06-05 17:28:46 +0700

    Phone Login Page UI Design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
+    implementation("com.hbb20:ccp:2.5.0")
     androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.ext.junit)
+    testImplementation(libs.junit)
 }
