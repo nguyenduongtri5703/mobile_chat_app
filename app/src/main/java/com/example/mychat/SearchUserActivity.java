53f15cc396845916bf7ac2fd719c56f5d5177292
Duong Tri Nguyen <21130456@st.hcmuaf.edu.vn>
2024-06-07 18:52:21 +0700

    Search User Page UI Design

--- app/src/main/java/com/example/mychat/SearchUserActivity.java
+++ app/src/main/java/com/example/mychat/SearchUserActivity.java
@@ -1,14 +1,22 @@
 package com.example.mychat;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SearchUserActivity extends AppCompatActivity {
+
+    EditText searchInput;
+    ImageButton searchButton;
+    ImageButton backButton;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,28 @@ public class SearchUserActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        searchInput = findViewById(R.id.search_username_input);
+        searchButton = findViewById(R.id.search_user_btn);
+        backButton = findViewById(R.id.back_btn);
+        recyclerView = findViewById(R.id.search_user_recycler_view);
+
+        searchInput.requestFocus();
+
+        backButton.setOnClickListener(v -> {
+            onBackPressed();
+        });
+
+        searchButton.setOnClickListener(v -> {
+            String searchTerm = searchInput.getText().toString();
+            if (searchTerm.isEmpty() || searchTerm.length() < 3) {
+                searchInput.setError("Người dùng không tồn tại");
+                return;
+            }
+            setupSearchRecyclerView(searchTerm);
+        });
+    }
+    void setupSearchRecyclerView(String searchTerm){
+
     }
 }
