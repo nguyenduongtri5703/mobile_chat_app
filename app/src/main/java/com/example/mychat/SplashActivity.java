6796147a82e508281cb71e90b1c7e3ec24612468
Loc203 <tranlocmom@gmail.com>
2024-06-07 16:21:44 +0700

    Save User Details in FirebaseFirestore

--- app/src/main/java/com/example/mychat/SplashActivity.java
+++ app/src/main/java/com/example/mychat/SplashActivity.java
@@ -10,6 +10,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.mychat.utils.FirebaseUtil;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
@@ -26,9 +28,13 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, LoginPhoneNumberActivity.class));
+                if (FirebaseUtil.isLoggedIn()){
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                } else {
+                    startActivity(new Intent(SplashActivity.this, LoginPhoneNumberActivity.class));
+                }
                 finish();
             }
-        }, 3000);
+        }, 1000);
     }
 }
