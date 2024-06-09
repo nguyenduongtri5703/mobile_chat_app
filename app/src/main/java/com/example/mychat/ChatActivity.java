10dabb2fff7492fb6fc3a0d2d31fc68e761bd062
Loc203 <tranlocmom@gmail.com>
2024-06-09 16:16:31 +0700

    Update User Profile

--- app/src/main/java/com/example/mychat/ChatActivity.java
+++ app/src/main/java/com/example/mychat/ChatActivity.java
@@ -67,6 +67,7 @@ public class ChatActivity extends AppCompatActivity {
 
         backBtn.setOnClickListener(v -> {
             Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
 //            onBackPressed();
         });
